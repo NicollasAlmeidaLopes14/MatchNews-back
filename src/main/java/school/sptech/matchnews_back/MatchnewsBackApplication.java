3e3bac054bb4de9769b0ffa5f219ab6e77a10130
NicollasAlmeidaLopes14 <nicollas.lopes@sptech.school>
2026-08-29 21:05:44 -0300

    chore: primeiro commit

---
+++ src/main/java/school/sptech/matchnews_back/MatchnewsBackApplication.java
@@ -0,0 +1,13 @@
+package school.sptech.matchnews_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchnewsBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchnewsBackApplication.class, args);
+	}
+
+}
